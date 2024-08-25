9c961a2b8e21d8c3142b3479aa088e7dc9a62e25
DaniBonica001 <danielabc_0106@hotmail.com>
2024-08-25 15:05:38 -0500

    initial commit

---
+++ src/main/java/co/analisys/miembroservice/MiembroserviceApplication.java
@@ -0,0 +1,13 @@
+package co.analisys.miembroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiembroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiembroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/analisys/miembroservice/MiembroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package co.analisys.miembroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiembroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
