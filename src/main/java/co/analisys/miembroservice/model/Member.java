8d41720f80d0e12a4c7e0fa3fe5a5f19ec16b919
G-Delgado <1006170256@u.icesi.edu.co>
2024-08-26 16:42:19 -0500

    Feat: modified pom.xml to add missing mapstruct dependency and added entity/table tag for h2 to work

--- src/main/java/co/analisys/miembroservice/model/Member.java
+++ src/main/java/co/analisys/miembroservice/model/Member.java
@@ -1,29 +1,35 @@
 package co.analisys.miembroservice.model;
 
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name = "members")
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Member {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
+
     private String name;
+
     private String email;
+
     private String phoneNumber;
+
     private boolean active;
+
     private LocalDate enrollmentDate;
 
 }
